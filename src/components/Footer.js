efc7abebb3867660247c64b04a71b6310b3fd0a7
rest2437 <nocturnalrd@gmail.com>
2021-10-18 13:10:36 -0700

    Added svg, footer, animation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,114 @@
 import React from "react";
+import "./Footer.css";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+// import smallLogo from "./photos/small-logo.png";
+// import teamName from "./photos/team.png";
 
-const Footer = () => {
+function Footer() {
   return (
-    <footer className="footer bg-dark">
-      <div className="container text-center">
-        <span className="text-muted">@2021 Built by SEI 802</span>
+    <div className="footer-container">
+      <section className="footer-subscription">
+        <p className="footer-subscription-heading">
+          Subscribe to get updates on new Products and Features!!
+        </p>
+        <p className="footer-subscription-text">
+          You can unsubscribe at any time.
+        </p>
+        <div className="input-areas">
+          <form className="footer-form">
+            <input
+              className="footer-input"
+              name="email"
+              type="email"
+              placeholder="Your Email"
+            />
+            <Button buttonStyle="btn--outline">Subscribe</Button>
+          </form>
+        </div>
+      </section>
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to="/about">About us</Link>
+          </div>
+          <div className="footer-link-items">
+            <h2>Contact Us</h2>
+            <Link to="/">Contact</Link>
+            <Link to="/">Questions</Link>
+            <Link to="/">Licenses</Link>
+          </div>
+        </div>
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>Videos</h2>
+            <Link to="/">Automotive projects</Link>
+          </div>
+          <div className="footer-link-items">
+            <h2>Social Media</h2>
+            <Link to="/">Instagram</Link>
+            <Link to="/">Facebook</Link>
+            <Link to="/">Youtube</Link>
+          </div>
+        </div>
       </div>
-    </footer>
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="footer-logo">
+            <Link to="/" className="social-logo">
+              {/* <img src={smallLogo} alt="" className="fab fa-typo3" /> */}
+              <i className="fab fa-typo3" />
+            </Link>
+          </div>
+          <small className="website-rights">RE Aerial Media SEI802 2021</small>
+          {/* <img src={teamName} alt="" className="fab fa-typo3" /> */}
+          <div className="social-icons">
+            <Link
+              className="social-icon-link facebook"
+              to="/"
+              target="_blank"
+              aria-label="Facebook"
+            >
+              <i className="fab fa-facebook-f" />
+            </Link>
+            <Link
+              className="social-icon-link instagram"
+              to="/"
+              target="_blank"
+              aria-label="Instagram"
+            >
+              <i className="fab fa-instagram" />
+            </Link>
+            <Link
+              className="social-icon-link youtube"
+              to="/"
+              target="_blank"
+              aria-label="Youtube"
+            >
+              <i className="fab fa-youtube" />
+            </Link>
+            <Link
+              className="social-icon-link twitter"
+              to="/"
+              target="_blank"
+              aria-label="Twitter"
+            >
+              <i className="fab fa-twitter" />
+            </Link>
+            <Link
+              className="social-icon-link twitter"
+              to="/"
+              target="_blank"
+              aria-label="LinkedIn"
+            >
+              <i className="fab fa-linkedin" />
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
   );
-};
+}
 
 export default Footer;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Logo from "./Logo";
 
 const Welcome = () => {
   return (
     <div>
+      <Logo />
       <h1>MERN Authentication</h1>
       <p>Welcome to my app.</p>
     </div>
