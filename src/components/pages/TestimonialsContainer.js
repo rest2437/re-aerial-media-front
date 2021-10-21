7c0b6a62b0b8895f329422ec29b7c00e36bbeb05
rest2437 <nocturnalrd@gmail.com>
2021-10-20 20:57:51 -0700

    added delete rout for testimonials

--- src/components/pages/TestimonialsContainer.js
+++ src/components/pages/TestimonialsContainer.js
@@ -1,10 +1,24 @@
+import axios from "axios";
 import React from "react";
+const { REACT_APP_SERVER_URL } = process.env;
 
 const TestimonialsContainer = (props) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    window.location.reload();
+
+    axios
+      .delete(`${REACT_APP_SERVER_URL}/testimonials/delete/${props._id}`)
+      .then((response) => {
+        console.log("===> DELETED");
+        console.log(response);
+      });
+  };
   return (
     <div className="each-post">
       <h2 id="name-of-test">{props.name}</h2>
       <p>{props.content}</p>
+      <button onClick={handleSubmit}>Delete</button>
     </div>
   );
 };
