87a5ceb854542564da6fa17305361f9e6779d52a
rest2437 <nocturnalrd@gmail.com>
2021-10-25 11:18:14 -0700

    ready for deployment

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Contact.css";
 import Peytan from "../images/peytan.jpg";
+const { REACT_APP_SERVER_URL } = process.env;
 
 const Contact = () => {
   const [status, setStatus] = useState("Submit");
